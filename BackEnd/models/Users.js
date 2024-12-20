eacce1eaee5642a8dae23756be41592a152763a4
shambhu <shambhu04.kumar@gmail.com>
2024-12-20 21:14:21 +0530

    add the BE files

--- BackEnd/models/Users.js
+++ BackEnd/models/Users.js
@@ -1,11 +1,29 @@
+import mongoose from "mongoose";
 import { Schema, model } from "mongoose";
 
 const userSchema = new Schema({
-  userName: { type: String, required: true },
-  email: { type: String },
-  password: { type: String, required: true },
+  userName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  workspaces: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Workspace",
+    },
+  ],
   phone: { type: String, required: true },
   refreshToken: { type: String },
+
 });
 
 export const User = new model("User", userSchema);
