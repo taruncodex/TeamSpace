1e1f8974cf231664336a82c7254324f1433314dc
Tarun Rathore <tarunrathore200@gmail.com>
2024-12-20 14:23:06 +0530

    Connecteion Between the backend and frontend is done , but getting something error

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -3,10 +3,14 @@ import "dotenv/config";
 import { connect } from "mongoose";
 import router from "./routes/routes.js";
 import cors from "cors";
+// import { User } from "./models/Users.js";
+// import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173" }));
+
 app.use(express.json());
 
 // error handling middle ware
@@ -16,11 +20,13 @@ app.use((err, req, res, next) => {
 
 app.use(router);
 
-app.listen(process.env.PORT, async () => {
+app.listen(process.env.PORT || 3000, async () => {
   try {
-    await connect(process.env.DATABASE_URL);
+    await connect(
+      "mongodb+srv://shambhu04kumar:123456mongodb@cluster0.auxyo.mongodb.net/workspaces"
+    );
     console.log("Database connected successfully");
-    console.log("Listening : http://localhost:3000 ");
+    console.log("Listening : http://localhost:3000");
   } catch (error) {
     console.log("Error :", error);
   }
